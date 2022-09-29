78b80f496c766e4700cb4c15d397720625ad083b
fonce1213 <110907605+fonce1213@users.noreply.github.com>
2022-09-29 22:26:45 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,74 @@
+class BooksController < ApplicationController
+
+  before_action :correct_user, only: [:edit, :update]
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+
+    if params[:id].present?
+      Book.find
+    else
+      @book = Book.new
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
+end
